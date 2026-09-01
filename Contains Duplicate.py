9513db93db13e1ab488d2f4d493325eceb15fc94
Eram Alim <95429684+eramalim@users.noreply.github.com>
2026-09-01 06:45:57 +0530

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -2,3 +2,13 @@
 You are given an array of integers nums. Return True if any value appears at least twice
 in the array, and False if every element is distinct.*/
 
+
+def contains_duplicate(nums):
+    seen = []
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.append(num)
+
+    return False
